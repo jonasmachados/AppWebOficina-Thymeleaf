5aa9d34115a2a5b7a5b218ec02dc26da74546e9c
jonasmachados <jonasmachado.ti@gmail.com>
2021-04-14 20:24:30 -0300

    Project Created0

---
+++ src/main/java/com/jonas/AppWebOficinaSpringThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.jonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebOficinaSpringThymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebOficinaSpringThymeLeafApplication.class, args);
+	}
+
+}
