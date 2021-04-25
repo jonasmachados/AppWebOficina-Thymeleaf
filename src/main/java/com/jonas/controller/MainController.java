f36839a13287185822a3cab47a2cea7e25eb5475
jonasmachados <jonasmachado.ti@gmail.com>
2021-04-25 16:25:11 -0300

    FRONTEND - Created

---
+++ src/main/java/com/jonas/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.jonas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+   
+}
