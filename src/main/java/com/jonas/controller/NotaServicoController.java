93dab74aa912d19443876be8ad321385c837f5c1
jonasmachados <jonasmachado.ti@gmail.com>
2021-05-23 12:08:29 -0300

    BACK/FRONT: Created method to add new NFe

--- src/main/java/com/jonas/controller/NotaServicoController.java
+++ src/main/java/com/jonas/controller/NotaServicoController.java
@@ -1,13 +1,17 @@
 
 package com.jonas.controller;
 
+import com.jonas.exception.RecordNotFoundException;
 import com.jonas.model.domain.NotaServico;
 import com.jonas.model.service.NotaServicoService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -24,7 +28,25 @@ public class NotaServicoController {
     @RequestMapping(value = "/notasServico")
     public String findAllNotasServico(Model model) {
         List<NotaServico> list = service.findAllNotasServico();
-        model.addAttribute("notasServico", list);
+        model.addAttribute("nfe", list);
         return "notaServico/list-notas";
+    }
+    
+    @RequestMapping(path = {"/editNFe", "/editNFe{id}"})
+    public String editNFeById(Model model, @PathVariable("id") Optional<Integer> id)
+            throws RecordNotFoundException {
+        if (id.isPresent()) {
+            NotaServico entity = service.getNotaServicoById(id.get());
+            model.addAttribute("nfe", entity);
+        } else {
+            model.addAttribute("nfe", new NotaServico());
+        }
+        return "notaServico/add-edit-nfe";
+    }
+    
+     @RequestMapping(path = "/createNFe", method = RequestMethod.POST)
+    public String createOrUpdateNFe(NotaServico nfe) {
+        service.createOrUpdateNFe(nfe);
+        return "redirect:/notasServico"; //REDIRECT: back to previous HTML.
     }
 }
--- src/main/java/com/jonas/model/service/NotaServicoService.java
+++ src/main/java/com/jonas/model/service/NotaServicoService.java
@@ -1,9 +1,11 @@
 package com.jonas.model.service;
 
+import com.jonas.exception.RecordNotFoundException;
 import com.jonas.model.domain.NotaServico;
 import com.jonas.repository.NotaServicoRepository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +26,41 @@ public class NotaServicoService {
             return result;
         } else {
             return new ArrayList<NotaServico>();
+        }
+    }
+    
+    public NotaServico createOrUpdateNFe(NotaServico entity) {
+        if (entity.getId() == null) {
+            entity = repository.save(entity);
+            return entity;
+        } else {
+            Optional<NotaServico> nfe = repository.findById(entity.getId());
+
+            if (nfe.isPresent()) {
+                NotaServico newNFe = nfe.get();
+                newNFe.setDescricao(entity.getDescricao());
+                newNFe.setPreco(entity.getPreco());
+                newNFe.setDataServico(entity.getDataServico());
+                newNFe.setKm(entity.getKm());
+
+                newNFe = repository.save(newNFe);
+
+                return newNFe;
+            } else {
+                entity = repository.save(entity);
+
+                return entity;
+            }
+        }
+    }
+
+    public NotaServico getNotaServicoById(Integer id) throws RecordNotFoundException {
+        Optional<NotaServico> nfe = repository.findById(id);
+
+        if (nfe.isPresent()) {
+            return nfe.get();
+        } else {
+            throw new RecordNotFoundException("No nfe record exist for given id");
         }
     }
 }
